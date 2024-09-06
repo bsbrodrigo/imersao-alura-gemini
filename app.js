6f561ec3ab79910bb23bf7824cf56e8412113189
unknown <rodriguinbsb@icloud.com>
2024-09-06 20:10:57 -0300

    finalizando o projeto

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 function pesquisar() {
-    // Declara uma função chamada 'pesquisar'
-    // Essa função é responsável por exibir os resultados da pesquisa na página
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+
+  if (!campoPesquisa) {
+      section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta.</p>"
+      return
+  }
   
-    let section = document.getElementById("resultados-pesquisa");
-    // Obtém a seção HTML com o ID "resultados-pesquisa"
-    // Essa seção será usada para inserir os resultados da pesquisa
-  
-    console.log(section);
-    // Imprime no console o elemento encontrado. Útil para depuração.
-  
-    // Itera sobre cada dado dentro da lista 'dados'
-    for (let dado of dados) {
-      // Para cada dado, cria um novo elemento HTML com a classe 'item-resultado'
-      // Esse elemento representa um resultado da pesquisa
-      section.innerHTML += `
-      <div class="item-resultado">
-                  <h2>
-                  <img src=${dado.imagem} class ="foto" alt "imagem">                
-                  <p class="descricao-meta">
-                      <a href="#" target="_blank">${dado.titulo}</a>
-                  </h2>
-                  ${dado.descricao}</p>
-                  <a href=${dado.link}target="_blank">Mais informações</a>
-              </div>
-  `
+
+  let resultados = "";
+
+  for (let dado of dados) {
+    let titulo = dado.titulo.toLocaleLowerCase();
+    let descricao = dado.descricao;
+    let tags = dado.tags; // Assumindo que tags é uma string
+    let imagem = dado.imagem;
+
+    // Verificar se a pesquisa está em algum dos campos
+    if (titulo.includes(campoPesquisa)) {
+      resultados += `
+        <div class="item-resultado">
+          <img src="${imagem}"></img>
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href=${dado.link} target="_blank">Mais informações</a>
+        </div>
+      `;
     }
+  }
+
+  if (!resultados) {
+      resultados = "<p>Nada foi encontrado.</p>"
+console.log(campoPesquisa)
+  }
+
+  section.innerHTML = resultados;
 }
-  // console.log(dados);
-
-
-
-
-
-
